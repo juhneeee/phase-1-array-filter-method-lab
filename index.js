482f5f310959a75d7406e22fd8f061bffb8cc1b6
Johnny Chen <johnnyc1314@gmail.com>
2022-04-05 23:12:10 -0400

    commit tree c664dd3f5a5e653978387229eead72405e52c665

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution here
+function findMatching(listDrivers, nameDriver){
+    let newArr = listDrivers.filter( element => element.toUpperCase() == nameDriver.toUpperCase());
+    return newArr
+}
+function fuzzyMatch(listDrivers,nameDriver){
+    let newArr = listDrivers.filter(obj => obj.slice(0,2).toUpperCase() === nameDriver.slice(0,2).toUpperCase());
+    return newArr
+}
+function matchName(xlistDrivers,nameDriver){
+    let newArr = xlistDrivers.filter(obj => obj['name'] === nameDriver)
+    return newArr
+}
+
+
+// let listDrivers = ['Abby','BriLLY', 'cralet']
+// findMatching(listDrivers, 'abby')
+let xlistDrivers = [
+    {name: 'abby', hometown: 'nyc'},
+    {name: 'abby', hometown: 'big apple'},
+    {name: 'brilly', hometown: 'nyc'}]
+[1, 2, 3, 4, 5].filter(function (num) {
+  return num > 3;
+});
+// => [4, 5]
